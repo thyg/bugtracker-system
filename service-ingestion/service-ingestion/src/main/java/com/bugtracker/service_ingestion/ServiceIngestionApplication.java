eb7e4091dee609a8bd607da09bf5c8ec8278dcde
jude <yimbouthedom@gmail.com>
2025-06-01 16:15:38 +0100

    ajout des services ingestion et traitement

---
+++ service-ingestion/service-ingestion/src/main/java/com/bugtracker/service_ingestion/ServiceIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.bugtracker.service_ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceIngestionApplication.class, args);
+	}
+
+}
